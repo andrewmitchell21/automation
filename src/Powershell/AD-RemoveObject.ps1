6eec5bc4ee999f36f85a41082747c896b51fc118
Andrew Mitchell <andrew@BGNEW068.local>
2016-07-10 17:32:19 +0100

    Added powershell scripts

---
+++ src/Powershell/AD-RemoveObject.ps1
@@ -0,0 +1,49 @@
+
+<#
+.SYNOPSIS
+    Removes computer object from AD
+.DESCRIPTION
+    This script will remove the computer object from AD based on the value passed in via the user
+.PARAMETER vmName
+    Full VM name for the computer object you wish to delete.
+.EXAMPLE
+    .\Ad-RemoveObject.ps1 -vmName "ew1-st01-250-01.ad.bedegaming.net"
+#>
+
+param
+(
+    [Parameter(Mandatory=$true)]   [string] [ValidateNotNullOrEmpty()]  $vmName
+)
+
+$ErrorActionPreference = "Stop";
+$separator ="."
+$adMachine = $vmName.split($separator,2)[0]
+
+try
+{
+    if ($adComputer = Get-ADComputer $adMachine)
+    {
+        $domainCheck = $adComputer.DistinguishedName
+        if($domainCheck.Split(',') -match '^ou=.*Domain Controllers')
+        {
+            Write-Output "Cannot remove computer $vmName as it belongs to the domain controller computer group"
+            exit(1)
+        }
+        else
+        {
+            Remove-ADObject -Identity $adComputer -Recursive -Confirm:$false
+            Write-Output "Removed AD object $adComputer"
+            exit(0)
+        }
+    }
+}
+catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]
+{
+    Write-Output "$vmName computer does not exist"
+    exit(1)
+}
+catch [Exception]
+{
+    Write-Output $_.Exception.Message
+    exit(1)
+}
---
+++ src/Powershell/DNS-RemoveRecord.ps1
@@ -0,0 +1,49 @@
+﻿<#
+.SYNOPSIS
+    Removes computer DNS record from the Domain Controller
+.DESCRIPTION
+    This script will remove a DNS from record for a given vm, please note you must pass the fully qualified domain name for the vm, E.g. ew1-st01-250-01.ad.bedegaming.net
+.PARAMETER vmName
+    Full VM name for the computer object you wish to delete.
+.EXAMPLE
+    .\DNS-RemoveDNSRecord.ps1 -vmName "ew1-st01-250-01.ad.bedegaming.net"
+.NOTES
+      Author: Andrew Mitchell
+#>
+
+param
+(
+    [Parameter(Mandatory=$true)]   [string] [ValidateNotNullOrEmpty()]  $vmName
+)
+
+$ErrorActionPreference = "Stop";
+$separator ="."
+$dnsName = $vmName.split($separator,2)[0]
+$dnsZoneName = $vmName.split($separator,2)[1]
+
+if (!$dnsZoneName )
+{
+    Write-Output "A fully qualified domain name must be supplied, aborting script."
+    exit(1)
+}
+
+try
+{
+    $dnsRecord = Get-DnsServerResourceRecord -ZoneName $dnsZoneName -Name $dnsName -ErrorAction Stop
+    if ($dnsRecord)
+    {
+      Remove-DnsServerResourceRecord -ZoneName $dnsZoneName -Name $dnsName -RRType $dnsRecord.RecordType -Confirm:$false -Force 
+      Write-Output "Removed DNS Record $dnsRecord"
+    }
+    else
+    {
+      Write-Output "DNS Record does not exist for $vmName"
+      exit(0)
+    }
+}
+
+catch [Exception]
+{
+    Write-Output $_.Exception.Message
+    exit(1)
+}
